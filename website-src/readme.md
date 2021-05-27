a5e20acbb318620553c99c798c8ece14bfc22560
tanaybhardwaj24 <tanaybhardwaj24@gmail.com>
2021-05-27 13:49:05 +0000

    rebasing

--- website-src/readme.md
+++
@@ -1,3 +0,0 @@
-# The Official Website For The Project
-
-### [Link](https://official-awesome-piracy.vercel.app/) 
--- website-src/src/alternate/server.js
+++
@@ -1,3 +0,0 @@
-var MarkdownIt = require('markdown-it'),
-    md = new MarkdownIt();
-var result = md.render('# markdown-it rulezz!');
